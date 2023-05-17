7cd6e606e3e5f75f16ff2498614ba61e54185c11
Tanner Rhines <tannerrhines@protonmail.com>
2023-05-17 00:58:48 -0800

    centered CSS, added HTML header, created JS timer when button is clicked

--- script.js
+++ script.js
@@ -3,7 +3,33 @@
 
 var startBtn = document.querySelector("button");
 
+var headerText = document.querySelector("h1");
 
-startBtn.addEventListener("click", function(){
-    console.log("clicked the button")
-})
+
+
+startBtn.addEventListener("click", function() {
+    let timeLeft = 10;
+    const timeElement = document.getElementById("time");
+  
+    function countdown() {
+      timeLeft--;
+      timeElement.innerHTML = String(timeLeft);
+      if (timeLeft > 0) {
+        setTimeout(countdown, 1000);
+      } else {
+        headerText.innerHTML = "Time is up!";
+      }
+    }
+  
+    setTimeout(countdown, 1000);
+  });
+
+
+
+
+
+
+
+
+
+
