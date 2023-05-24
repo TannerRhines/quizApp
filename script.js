8d563354df23dc2e18972965372c13c7506b376e
Tanner Rhines <tannerrhines@protonmail.com>
2023-05-24 13:41:56 -0800

    added more questions, innerText under HR

--- script.js
+++ script.js
@@ -2,14 +2,12 @@ var startBtn = document.querySelector("#startQuiz");
 var headerText = document.querySelector("h1");
 var question = document.querySelector("#question");
 var answerButtonsElement = document.getElementById('answer-btn');
+var currentQuestionIndex = 0;
 
-
-
-
-
+var lastQA = document.querySelector("#lastQuestionAnswer")
 
 startBtn.addEventListener("click", function() {
-  let timeLeft = 70;
+  let timeLeft = 10;
   const timeElement = document.getElementById("time");
   startBtn.style.display = "none";
   headerText.style.display = "none";
@@ -30,31 +28,46 @@ startBtn.addEventListener("click", function() {
   showQuestion();
 });
 
-
-
-
-
-
 function showQuestion() {
   var questionText = document.getElementById("question");
-  questionText.innerText = questions[0].question;
+  questionText.innerText = questions[currentQuestionIndex].question;
 
   var answerButtonsHTML = '';
-  questions[0].answers.forEach(answer => {
+  questions[currentQuestionIndex].answers.forEach(answer => {
     answerButtonsHTML += '<button class="btn">' + answer.text + '</button>';
   });
   answerButtonsElement.innerHTML = answerButtonsHTML;
+
+  var answerButtons = answerButtonsElement.querySelectorAll('.btn');
+  answerButtons.forEach(button => {
+    button.addEventListener('click', selectAnswer);
+  });
 }
 
+function selectAnswer(event) {
+  var selectedButton = event.target;
+  var correct = questions[currentQuestionIndex].answers.some(answer => answer.text === selectedButton.innerText && answer.correct);
+  if (correct) {
+    // Handle correct answer logic here
+    lastQA.innerText = "Correct";
+  } else {
+    // Handle wrong answer logic here
+    lastQA.innerText = "Incorrect";
+  }
 
+  // Increase the currentQuestionIndex to move to the next question
+  currentQuestionIndex++;
 
-
-
-
-
-
-
-
+  // Check if there are more questions
+  if (currentQuestionIndex < questions.length) {
+    // If there are more questions, display the next question
+    setTimeout(showQuestion, 1000);
+  } else {
+    // If there are no more questions, display a message or perform any other actions
+    headerText.innerHTML = "Quiz completed!";
+    answerButtonsElement.innerHTML = ""; // Clear the answer buttons
+  }
+}
 
 const questions = [
   {
@@ -74,8 +87,24 @@ const questions = [
       { text: 'Berlin', correct: false },
       { text: 'Madrid', correct: false }
     ]
+  },
+  {
+    question: "What is CSS?",
+    answers: [
+      { text: 'Cascading Style Sheets', correct: true },
+      { text: 'Unsure', correct: false },
+      { text: 'Machine Learning Language', correct: false },
+      { text: 'AWS App', correct: false }
+    ]
+  },
+  {
+    question: "What is Javascript used for?",
+    answers: [
+      { text: 'web dev', correct: true },
+      { text: 'gaming', correct: false },
+      { text: 'styling', correct: false },
+      { text: 'robotics', correct: false }
+    ]
   }
 ];
-
-
 
