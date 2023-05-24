d1b06278a23d328c76fa11438e0c599c39cfe5d9
Tanner Rhines <tannerrhines@protonmail.com>
2023-05-24 10:29:37 -0800

    added questions and answers array

--- script.js
+++ script.js
@@ -1,38 +1,34 @@
-
-
-
 var startBtn = document.querySelector("#startQuiz");
-
 var headerText = document.querySelector("h1");
-
 var question = document.querySelector("#question");
+var answerButtonsElement = document.getElementById('answer-btn');
 
-var answers = document.querySelector("#answer-btn");
+
+
 
 
 
 startBtn.addEventListener("click", function() {
-    let timeLeft = 10;
-    const timeElement = document.getElementById("time");
-    startBtn.style.display = "none";
-    headerText.style.display = "none";
+  let timeLeft = 70;
+  const timeElement = document.getElementById("time");
+  startBtn.style.display = "none";
+  headerText.style.display = "none";
+  question.style.display = "block";
+  answerButtonsElement.style.display = "block";
 
-    question.style.display = "block";
-    answers.style.display = "block";
-
-  
-    function countdown() {
-      timeLeft--;
-      timeElement.innerHTML = String(timeLeft);
-      if (timeLeft > 0) {
-        setTimeout(countdown, 1000);
-      } else {
-        headerText.innerHTML = "Time is up!";
-      }
+  function countdown() {
+    timeLeft--;
+    timeElement.innerHTML = String(timeLeft);
+    if (timeLeft > 0) {
+      setTimeout(countdown, 1000);
+    } else {
+      headerText.innerHTML = "Time is up!";
     }
-  
-    setTimeout(countdown, 1000);
-  });
+  }
+
+  setTimeout(countdown, 1000);
+  showQuestion();
+});
 
 
 
@@ -40,6 +36,36 @@ startBtn.addEventListener("click", function() {
 
 
 
+
+
+
+
+
+
+
+
+
+
+const questions = [
+  {
+    question: "What is an apple?",
+    answers: [
+      { text: 'fruit', correct: true },
+      { text: 'vegetable', correct: false },
+      { text: 'rock', correct: false },
+      { text: 'animal', correct: false }
+    ]
+  },
+  {
+    question: "What is the capital of France?",
+    answers: [
+      { text: 'Paris', correct: true },
+      { text: 'London', correct: false },
+      { text: 'Berlin', correct: false },
+      { text: 'Madrid', correct: false }
+    ]
+  }
+];
 
 
 
