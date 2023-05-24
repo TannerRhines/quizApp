db63f543ee77c540722fc1a4e36c40c8e9df6470
Tanner Rhines <tannerrhines@protonmail.com>
2023-05-24 08:37:55 -0800

    added question and answer id/class, tested display of answer buttons

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
 
 
-var startBtn = document.querySelector("button");
+var startBtn = document.querySelector("#startQuiz");
 
 var headerText = document.querySelector("h1");
+
+var question = document.querySelector("#question");
+
+var answers = document.querySelector("#answer-btn");
 
 
 
@@ -11,6 +15,11 @@ startBtn.addEventListener("click", function() {
     let timeLeft = 10;
     const timeElement = document.getElementById("time");
     startBtn.style.display = "none";
+    headerText.style.display = "none";
+
+    question.style.display = "block";
+    answers.style.display = "block";
+
   
     function countdown() {
       timeLeft--;
