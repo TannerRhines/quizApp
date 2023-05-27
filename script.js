639828414170e0a89054f7c4b04e22f40f6b8ac9
Tanner Rhines <tannerrhines@protonmail.com>
2023-05-26 23:55:56 -0800

    refactored/ removed errors

--- script.js
+++ script.js
@@ -1,144 +1,69 @@
-var startBtn = document.querySelector("#startQuiz");
-var headerText = document.querySelector("h1");
-var question = document.querySelector("#question");
-var answerButtonsElement = document.getElementById('answer-btn');
-var currentQuestionIndex = 0;
-var lastQA = document.querySelector("#lastQuestionAnswer");
-var currentScore = document.querySelector("#currentScore");
-var point = 10;
-var timeLeft = 30; // Declare timeLeft as a global variable
+const initialCountdown = 70;
+const timePenalty = 10;
 
-let countdownTimeout; // Declare timeout variable
+let timer;
+let score;
+let currentQuestionIndex;
+let intervalId;
 
-startBtn.addEventListener("click", function() {
-  const timeElement = document.getElementById("time");
-  startBtn.style.display = "none";
-  headerText.style.display = "none";
-  question.style.display = "block";
-  answerButtonsElement.style.display = "block";
-
-  function countdown() {
-    timeLeft--;
-    timeElement.innerHTML = String(timeLeft);
-    if (timeLeft > 0) {
-      countdownTimeout = setTimeout(countdown, 1000);
-    } else {
-      headerText.style.display = "block";
-      headerText.innerHTML = "Time is up!";
-      currentQuestionIndex.style.display = "none";
-    }
-  }
-
-  countdownTimeout = setTimeout(countdown, 1000);
-  showQuestion();
-});
-
-function showQuestion() {
-  var questionText = document.getElementById("question");
-  questionText.innerText = questions[currentQuestionIndex].question;
-
-  var answerButtonsHTML = '';
-  questions[currentQuestionIndex].answers.forEach(answer => {
-    answerButtonsHTML += '<button class="btn">' + answer.text + '</button>';
-  });
-  answerButtonsElement.innerHTML = answerButtonsHTML;
-
-  var answerButtons = answerButtonsElement.querySelectorAll('.btn');
-  answerButtons.forEach(button => {
-    button.addEventListener('click', selectAnswer);
-  });
-
-  if (timeLeft === 0) {
-    questionText.style.display = "none";
-
-  }
-}
-
-
-function selectAnswer(event) {
-  var selectedButton = event.target;
-  var correct = questions[currentQuestionIndex].answers.some(answer => answer.text === selectedButton.innerText && answer.correct);
-  if (correct) {
-    // Handle correct answer logic here
-    lastQA.innerText = "Correct";
-    currentScore.innerHTML = "Score: " + point;
-  } else {
-    // Handle wrong answer logic here
-    lastQA.innerText = "Incorrect";
-    timeLeft -= 10;
-  }
-
-  // Increase the currentQuestionIndex to move to the next question
-  currentQuestionIndex++;
-
-  // Check if we've gone through all the questions
-  if(currentQuestionIndex == questions.length){
-    quizOver();
-  } else {
-    // Display the next question
-    showQuestion();
-  }
-}
-
-
-
-function quizOver() {
-  // Quiz is completed, perform actions here
-  clearTimeout(countdownTimeout); // Clear the countdown
-  headerText.style.display = "block";  // Set the display property to block
-  headerText.innerHTML = "Quiz completed!";
-  answerButtonsElement.innerHTML = ""; // Clear the answer buttons
-  // You can calculate the final score or display a message, etc.
-  questionText.style.display = "none";
-}
+var startBtn = document.getElementById('start');
+var quiz = document.getElementById('quiz');
+var questionContainer = document.getElementById('question-container');
+var countdownEl = document.getElementById('countdown');
+var currentScoreEl = document.getElementById('current-score');
+var questionEl = document.getElementById('question');
+var answersEl = document.getElementById('answers');
+var finalScoreEl = document.getElementById('final-score');
+var resultEl = document.getElementById('result');
+var initialsEl = document.getElementById('initials');
+var saveScoreBtn = document.getElementById('save-score');
+var timeOutMessageEl = document.getElementById('time-out-message');
+var highscoresLink = document.getElementById('highscores-link');
+var highscoresDisplay = document.getElementById('highscores-display');
+var highscoresListEl = document.getElementById('highscores-list');
+var feedbackEl = document.getElementById('feedback');
+var backLink = document.getElementById('back-link');
 
 
 
 const questions = [
+  // Your questions and answers go here
   {
-    question: "What is an apple?",
-    answers: [
+  question: "What is an apple?",
+  answers: [
       { text: 'fruit', correct: true },
       { text: 'vegetable', correct: false },
       { text: 'rock', correct: false },
       { text: 'animal', correct: false }
-    ]
-  },
-  {
-    question: "What is the capital of France?",
-    answers: [
+  ]
+},
+{
+  question: "What is the capital of France?",
+  answers: [
       { text: 'Paris', correct: true },
       { text: 'London', correct: false },
       { text: 'Berlin', correct: false },
       { text: 'Madrid', correct: false }
-    ]
-  },
-  {
-    question: "What is CSS?",
-    answers: [
-      { text: 'Cascading Style Sheets', correct: true },
-      { text: 'Unsure', correct: false },
-      { text: 'Machine Learning Language', correct: false },
-      { text: 'AWS App', correct: false }
-    ]
-  },
-  {
-    question: "What is Javascript used for?",
-    answers: [
-      { text: 'web dev', correct: true },
-      { text: 'gaming', correct: false },
-      { text: 'styling', correct: false },
-      { text: 'robotics', correct: false }
-    ]
-  },
-  {
-    question: "What does JSON do?",
-    answers: [
-      { text: 'web dev', correct: true },
-      { text: 'gaming', correct: false },
-      { text: 'styling', correct: false },
-      { text: 'robotics', correct: false }
-    ]
-  }
+  ]
+},
+{
+  question: "What is lettuce?",
+  answers: [
+      { text: 'fruit', correct: false },
+      { text: 'vegetable', correct: true },
+      { text: 'rock', correct: false },
+      { text: 'animal', correct: false }
+  ]
+},
+{
+  question: "What is the capital of Russia?",
+  answers: [
+      { text: 'Paris', correct: false },
+      { text: 'London', correct: false },
+      { text: 'Berlin', correct: false },
+      { text: 'Moscow', correct: true }
+  ]
+},
+
 ];
 
